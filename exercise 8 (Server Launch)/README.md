2d0f82e51f75468bd0fbd94d87e47bb5fe651106
oluwatobiiloba <74077388+oluwatobiiloba@users.noreply.github.com>
2022-10-17 08:05:47 +0100

    task completed

---
+++ exercise 8 (Server Launch)/README.md
@@ -0,0 +1,14 @@
+# altschool-cloud-exercises
+
+*Altschool learning cloud track*
+
+1. systemctl
+
+![cp](systemctl%20.png)
+
+2. Sample page
+
+![cp](index%20_page.png)
+
+
+
---
+++ exercise 8 (Server Launch)/index.php
@@ -0,0 +1,11 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Hello, World!</title>
+</head>
+<body>
+        <h1><?php echo 'Hello, World!'; ?></h1>
+	<h2><?php echo date("F d, Y h:i:s A e", time()); ?></h2>
+</body>
+</html>
+
